03a4c2c6ea53f1c8b80c992a3f5d589c9bf68ed4
somar sabe <sabesofteng@gmail.com>
2023-12-09 10:46:14 +0400

    klh

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
-const port = 5000; 
+const port = process.env.PORT || 5000; 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow requests from this origin
+  origin: 'https://alhproperties-ry5d.vercel.app/', // Allow requests from this origin
   methods: ['GET', 'POST'], // Allow only specified methods
   credentials: true, // Allow including cookies in requests (if using sessions/cookies)
 }));
